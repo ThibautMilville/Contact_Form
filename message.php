5cf8185006f4588263bd482f82ff23eb720e0c52
ThibautMilville <tmilville.pro@gmail.com>
2023-03-24 18:36:46 +0100

    debug

--- message.php
+++ message.php
@@ -7,8 +7,17 @@
     $message = $_POST['message'];
 
     if(!empty($email) && !empty($message)){ // If email and message fields are not empty
-        if(filter_var($email, FILTER_VALIDATE_EMAIL)){
-
+        if(filter_var($email, FILTER_VALIDATE_EMAIL)){ // If user entered email is valid
+            $receiver = "tmilville.test@gmail.com";
+            $subject = "From $name <$email>"; //Subject of the email : "From: Thibaut <abc@gmail.com>"
+            // Concatened chain for the email's body
+            $body = "Name: $name\nEmail: $email\nPhone: $phone\nWebsite: $website\nMessage: $message\n\nRegards,\n$name";
+            $sender = "From: $email";
+            if(mail($receiver, $subject, $body, $sender)){ // mail() is an inbuilt php function to send email
+                echo "Your message has been sent!";
+            }else{
+                echo "Sorry, failed to send your message!";
+            }
         }else{
             echo "Enter a valid email address!";
         }
--- script.js
+++ script.js
@@ -5,6 +5,7 @@ statusTxt = form.querySelector(".button-area span")
 
 form.onsubmit = (e)=>{
     e.preventDefault() // Preventing form from submitting
+    statusTxt.style.color = "#0d6efd"
     statusTxt.style.display = "block"
     
     let xhr = new XMLHttpRequest() // Creating new XML object
@@ -12,7 +13,16 @@ form.onsubmit = (e)=>{
     xhr.onload = ()=>{ //Once ajax loaded
         if(xhr.readyState == 4 && xhr.status == 200){ // If ajax response is 200 & ready status is 400 means there is no any error
             let response = xhr.response // Storing ajax response in a response variable
-            console.log(response)
+            // If response is an error then we change status color to red, else we reset the form
+            if(response.indexOf("Email and message fields are required!") != -1 || response.indexOf("Enter a valid email address!") || response.indexOf("Sorry, failed to send your message!")){
+                statusTxt.style.color = "red"
+            }else{
+                console.log("coucou")
+                form.reset()
+                setTimeout(()=>{
+                    statusTxt.style.display = "none"
+                }, 3000) // Hide the status after 3 seconds if the form is sent
+            }
             statusTxt.innerText = response
         }
     }
