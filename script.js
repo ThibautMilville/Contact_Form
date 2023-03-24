a2803e3398e0d3d73bedfece2c9a9c94f08f7ef1
ThibautMilville <tmilville.pro@gmail.com>
2023-03-24 14:43:17 +0100

    correction_bug

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ form.onsubmit = (e)=>{
     e.preventDefault() // Preventing form from submitting
     statusTxt.style.display = "block"
     
-    let xhr = new XMLHttpsRequest() // Creating new XML object
+    let xhr = new XMLHttpRequest() // Creating new XML object
     xhr.open("POST", "message.php", true) // Sending post request to message.php file
     xhr.onload = ()=>{ //Once ajax loaded
         if(xhr.readyState == 4 && xhr.status == 200){ // If ajax response is 200 & ready status is 400 means there is no any error
